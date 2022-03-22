078b2683b4dd4259a03ca7ac01b42790c5fe0ba5
Chulainn1 <leam.murphy1@gmail.com>
2022-03-22 00:09:10 -0600

    cleaned up code. created hooks file. deleted class component. deleted the functions that made gql request using apollo client directly.

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,46 +1,16 @@
-import { useQuery, useMutation, useSubscription } from "@apollo/react-hooks";
 import React from "react";
-import client from "./graphql/client";
-import {
-  addMessageMutation,
-  messagesQuery,
-  messageAddedSubscription,
-} from "./graphql/queries";
+import { useChatMessages } from "./hooks";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 
-function useChatMessages() {
-  const { data } = useQuery(messagesQuery);
-  const messages = data ? data.messages : [];
-  useSubscription(messageAddedSubscription, {
-    onSubscriptionData: ({ client, subscriptionData }) => {
-      client.writeData({
-        data: {
-          messages: messages.concat(subscriptionData.data.messageAdded),
-        },
-      });
-    },
-  });
-  const [addMessage] = useMutation(addMessageMutation);
-  return {
-    messages,
-    addMessage,
-  };
-}
-
 const Chat = ({ user }) => {
   const { messages, addMessage } = useChatMessages();
-
-  const handleSend = async (text) => {
-    const { data } = await addMessage({ variables: { input: { text } } });
-    console.log("mutation data", data);
-  };
   return (
     <section className="section">
       <div className="container">
         <h1 className="title">Chatting as {user}</h1>
         <MessageList user={user} messages={messages} />
-        <MessageInput onSend={handleSend} />
+        <MessageInput onSend={addMessage} />
       </div>
     </section>
   );
---
+++ client/src/hooks.js
@@ -0,0 +1,24 @@
+import { useQuery, useMutation, useSubscription } from "@apollo/react-hooks";
+import {
+  addMessageMutation,
+  messagesQuery,
+  messageAddedSubscription,
+} from "./graphql/queries";
+export function useChatMessages() {
+  const { data } = useQuery(messagesQuery);
+  const messages = data ? data.messages : [];
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({ client, subscriptionData }) => {
+      client.writeData({
+        data: {
+          messages: messages.concat(subscriptionData.data.messageAdded),
+        },
+      });
+    },
+  });
+  const [addMessage] = useMutation(addMessageMutation);
+  return {
+    messages,
+    addMessage: (text) => addMessage({ variables: { input: { text } } }),
+  };
+}
