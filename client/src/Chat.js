d7b58fcf81f32c627850d57a6628de0673dfb16a
Chulainn1 <leam.murphy1@gmail.com>
2022-03-21 23:37:58 -0600

    implemented useMutation hook, which is passed addMessageMutation. this hook returns an arr, where the first elem is a function that will execute the mutation.

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,20 +1,22 @@
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import React, { useState } from "react";
 import client from "./graphql/client";
-import { messagesQuery } from "./graphql/queries";
+import { addMessageMutation, messagesQuery } from "./graphql/queries";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 
 const Chat = ({ user }) => {
   const { data } = useQuery(messagesQuery);
+  const [addMessage] = useMutation(addMessageMutation);
   // const [result, setResult] = useState({ loading: true });
   // client
   //   .query({ query: messagesQuery })
   //   .then(({ data }) => setResult({ loading: false, data }));
   const messages = data ? data.messages : [];
 
-  const handleSend = (text) => {
-    // TODO
+  const handleSend = async (text) => {
+    const { data } = await addMessage({ variables: { input: { text } } });
+    console.log("mutation data", data);
   };
   return (
     <section className="section">
