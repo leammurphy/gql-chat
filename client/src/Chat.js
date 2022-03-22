6d93f85f8e72ee896a7e9c2f502025f94edd5d44
Chulainn1 <leam.murphy1@gmail.com>
2022-03-21 23:57:45 -0600

    using local state management with apollo client. within the useSubscription hook wrote a obj with a data property directly to the cache. useQuery fetches from the server and responds to local updates to the cache.data written directly into the cache it must have the same structure as in the query used to retrieve that data

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useSubscription } from "@apollo/react-hooks";
-import React, { useState } from "react";
+import React from "react";
 import client from "./graphql/client";
 import {
   addMessageMutation,
@@ -10,20 +10,18 @@ import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 
 const Chat = ({ user }) => {
-  const [messages, setMessages] = useState([]);
-  useQuery(messagesQuery, {
-    onCompleted: ({ messages }) => setMessages(messages),
-  });
+  const { data } = useQuery(messagesQuery);
+  const messages = data ? data.messages : [];
   useSubscription(messageAddedSubscription, {
-    onSubscriptionData: ({ subscriptionData }) => {
-      setMessages(messages.concat(subscriptionData.data.messageAdded));
+    onSubscriptionData: ({ client, subscriptionData }) => {
+      client.writeData({
+        data: {
+          messages: messages.concat(subscriptionData.data.messageAdded),
+        },
+      });
     },
   });
   const [addMessage] = useMutation(addMessageMutation);
-  // const [result, setResult] = useState({ loading: true });
-  // client
-  //   .query({ query: messagesQuery })
-  //   .then(({ data }) => setResult({ loading: false, data }));
 
   const handleSend = async (text) => {
     const { data } = await addMessage({ variables: { input: { text } } });
